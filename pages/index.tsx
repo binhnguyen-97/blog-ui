7c932954dabe38c73bef364ced05eec85ed47854
binhnguyen <nttbinh@fossil.com>
2021-09-18 23:21:19 +0700

    chore: update og tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,23 +38,35 @@ const HomePage = ({
         <title>BiDu&apos;s Blog</title>
         <meta
           name="description"
-          content="BiDu blog là nơi chia sẻ những bài viết xoay quanh chủ đề về cuộc sống và công nghệ."
-        />
-        <meta
-          name="og:description"
           content="BiDu blog là nơi chia sẻ những bài viết xoay quanh chủ đề về cuộc sống và công nghệ. Các bài viết đều dựa theo những trải nghiệm và nghiên cứu thức tế của bọn mình."
         />
         <meta
+          name="image"
+          content={`${process.env.BASE_URL}/slogan-logo.svg`}
+        />
+        <meta
+          name="og:description"
+          property="BiDu blog là nơi chia sẻ những bài viết xoay quanh chủ đề về cuộc sống và công nghệ. Các bài viết đều dựa theo những trải nghiệm và nghiên cứu thức tế của bọn mình."
+        />
+        <meta
           name="og:title"
-          content="BiDu&apos;s Blog - Life and Technology"
+          property="BiDu&apos;s Blog - Life and Technology"
         />
         <meta
           name="og:image"
-          content="/slogan-logo.svg"
+          property={`${process.env.BASE_URL}/slogan-logo.svg`}
         />
         <meta
           name="og:url"
-          content={process.env.NEXT_PUBLIC_BASE_URL}
+          property={process.env.NEXT_PUBLIC_BASE_URL}
+        />
+        <meta
+          name="og:type"
+          property="article/listing"
+        />
+        <meta
+          name="fb:app_id"
+          property="248079427097756"
         />
       </Head>
       <Home articleList={articleList} highlightArticles={highlightArticles} />
